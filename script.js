c2f0beeee343a036968009bf96758b669c7c1119
xiaowu <xiaowu.ruan@lofty.com>
2024-05-08 14:14:23 +0800

    feat: add some code

--- script.js
+++ script.js
@@ -530,3 +530,35 @@ Calendly.initBadgeWidget(
   };
   document.body.appendChild(script);
 })();
+
+// AM-430259 xiaowu.ruan 2024.05.08
+(function () {
+  if (window.location.hash.indexOf("homes-for-sale-in") > -1) {
+    const script = document.createElement("script");
+    script.src = "https://www.googletagmanager.com/gtag/js?id=AW-16489748952";
+    script.async = true;
+    document.body.appendChild(script);
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+    gtag("config", "AW-16489748952");
+    gtag("config", "AW-16489748952/1fpRCP6ilqoZENiz9rY9", {
+      phone_conversion_number: "[+1(916) 302-7655|tel:+19163027655]",
+    });
+    function gtag_report_conversion(url) {
+      var callback = function () {
+        if (typeof url != "undefined") {
+          window.location = url;
+        }
+      };
+      gtag("event", "conversion", {
+        send_to: "AW-16489748952/K8zzCPam_6kZENiz9rY9",
+        event_callback: callback,
+      });
+      return false;
+    }
+  }
+})();
